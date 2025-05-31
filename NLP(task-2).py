9cc88009895ea31f708886c451acfaedf7f954df
Khushal Prajapati <prajapatikhush990@gmail.com>
2025-05-31 16:16:47 +0530

    Add files via upload

---
+++ NLP(task-2).py
@@ -0,0 +1,30 @@
+from textblob import TextBlob
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+def analyze_sentment_textblob(text):
+    sentiment = TextBlob(text).sentiment.polarity
+
+    if sentiment > 0:
+        return "positive 😊"
+    elif sentiment < 0:
+        return "negative 😡"
+    else:
+        return "neutral 😐"
+
+def analyze_sentiment_vader(text):
+    analyzer = SentimentIntensityAnalyzer()
+    sentiment = analyzer.polarity_scores(text)['compound']
+
+    if sentiment > 0.05:
+        return "positive😊"
+    elif sentiment < -0.05:
+        return "negative😡"
+    else:
+        return "neutral😐"
+
+def analyze_input():
+    text = input("Enter a sentence for sentiment analysis: ")
+    print(f"TextBlob Sentiment: {analyze_sentment_textblob(text)}")
+    print(f"Vader Sentiment: {analyze_sentiment_vader(text)}")
+# Run the function
+analyze_input()
